b6cc8aebf2d76f4ae3d58f31387a01428f00bcb2
Caleb Andreano <calebandreano@gmail.com>
2024-02-10 15:33:11 -0600

    Recv Partial Implementation:
    - recv stores data on edges, not on timeout. This should be fixed.
    - .ino sketch for xiaoduino board, sends a hardcoded "test" on pin 5
    on a button press on pin 1.

---
+++ send_test_manchester.ino/send_test_manchester.ino.ino
@@ -0,0 +1,64 @@
+// Constants won't change. They're used here to set pin numbers:
+const int buttonPin = 0;  // The number of the pushbutton pin
+const int ledPin = 1;     // The number of the LED pin
+const int txPin = 5;
+int counter = 0;
+
+
+// Variables will change:
+int buttonState = 0;       // Variable for reading the pushbutton status
+int lastButtonState = LOW; // Variable to track the previous button state
+unsigned long lastDebounceTime = 0;  // The last time the output pin was toggled
+unsigned long debounceDelay = 50;    // The debounce time; increase if the output flickers
+
+unsigned long previousMillis = 0;  // Store the last time output was updated
+const unsigned long interval = 1000;
+
+int buffer[] = {1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0};
+
+void setup() {
+  // Initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // Initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  pinMode(txPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Read the state of the pushbutton value:
+  int reading = digitalRead(buttonPin);
+
+  // Check if the button state has changed since the last check
+  if (reading != lastButtonState) {
+    // Reset the debounce timer
+    lastDebounceTime = millis();
+  }
+
+  // Check if the button state has remained stable for the debounce delay
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // If the reading is different from the current button state
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // If the button state is HIGH (pressed)
+      if (buttonState == HIGH) {
+        // Increment counter and print the value
+        Serial.printf("Pressed: %d\n", counter++);
+        // Turn on the LED
+        digitalWrite(ledPin, HIGH);
+        for (int i = 0; i < 64; i++) {
+            digitalWrite(txPin, buffer[i]);
+            delayMicroseconds(500); // Adjust this value to change the frequency (500 microseconds for 1 kHz)
+          }
+        digitalWrite(txPin, HIGH);
+      } else {
+        // Turn off the LED
+        digitalWrite(ledPin, LOW);
+      }
+    }
+  }
+
+  // Update lastButtonState with the current buttonState
+  lastButtonState = reading;
+}
